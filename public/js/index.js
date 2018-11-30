b14236f435e60ff48e0f9e110c6a77ffcbe25fb6
Brandon <BmcPrimary@yahoo.com>
2018-11-30 16:53:21 -0500

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,22 @@ socket.on('newMessage', function(message) {
     document.querySelector('#messages').insertAdjacentHTML('beforeend', li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    let li = document.createElement('li');
+    let lText = document.createTextNode(`${message.from}`);
+    li.appendChild(lText);
+
+    let a = document.createElement('a');
+    a.setAttribute('target', '_blank');
+    a.setAttribute('href', `${message.url}`);
+    let aText = document.createTextNode('My current location');
+    a.appendChild(aText);
+    li.appendChild(a);
+    document.querySelector('#messages').insertAdjacentHTML('beforeend', li.innerHTML);
+    
+
+});
+
 document.querySelector('#message-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -22,5 +38,22 @@ document.querySelector('#message-form').addEventListener('submit', function(e) {
         text: document.querySelector('input[name=message]').value
     }, function() {
 
+    });
+});
+
+let locationButton = document.querySelector('#send-location');
+
+locationButton.addEventListener('click', function(e) {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location.')
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('Generate Message', () => {
     it('Should generate the correct message object', () => {
@@ -10,5 +10,18 @@ describe('Generate Message', () => {
         let message = generateMessage(from, text);
         expect(typeof message.createdAt).toBe("number");
         expect(message).toMatchObject({from, text});
+    });
+});
+
+describe('Generate Location Message', () => {
+    it('Should generate correct location object', () => {
+        let from = 'Jim';
+        let lat = 500;
+        let long = 1000;
+
+        let locationMessage = generateLocationMessage(from, lat, long);
+        expect(locationMessage.from).toMatch(from);
+        expect(locationMessage.url).toMatch(`https://www.google.com/maps?q=${lat},${long}`);
+        expect(typeof locationMessage.createdAt).toBe("number");
     });
 });
